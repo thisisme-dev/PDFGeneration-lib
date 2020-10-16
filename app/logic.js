8073eb2a2958d89d5b04e76f3a3cb02a18d8e000
Herman <vanzyl.herman26@gmail.com>
2020-10-16 21:41:50 +0200

    AWS_DEFAULT_REGION reference changed to TIM_AWS_DEFAULT_REGION

--- app/logic.js
+++ app/logic.js
@@ -6,7 +6,7 @@ const bwipjs = require('bwip-js');
 
 const AWS = require('aws-sdk');
 const s3 = new AWS.S3();
-AWS.config.update({region: process.env.AWS_DEFAULT_REGION});
+AWS.config.update({region: process.env.TIM_AWS_DEFAULT_REGION});
 const AWS_S3_REPORTS_BUCKET = process.env.AWS_S3_REPORTS_BUCKET;
 
 const constants = require('./constants');
