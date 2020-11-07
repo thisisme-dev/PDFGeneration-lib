b982bb514c082c91462343a7eafbeeebb3919f68
Monty Furmie <armondfurmie@gmail.com>
2020-11-07 10:04:58 +0200

    Adding stubs to support custom font selection and sizes. Adding functionality for side by side images

--- app/line-types/header-line.js
+++ app/line-types/header-line.js
@@ -4,11 +4,11 @@ module.exports = {
   generateLineThatIsHeader,
 };
 
-function generateLineThatIsHeader(doc, x, y, isFancyHeader, text, incrementY, headerColor, getDocY) {
+function generateLineThatIsHeader(doc, x, y, isFancyHeader, text, incrementY, headerColor, getDocY, font) {
   const docY = getDocY(doc, y, incrementY, 1, false);
   doc = docY.doc;
   y = docY.y;
-  doc = sectionTypeLogic.populateHeaderLine(doc, headerColor, text, '', x, 180, y, isFancyHeader);
+  doc = sectionTypeLogic.populateHeaderLine(doc, headerColor, text, '', x, 180, y, isFancyHeader, font);
   if (!isFancyHeader) {
     y += incrementY + incrementY/2;
   }
